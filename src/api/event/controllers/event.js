a350ebf31d0108a6f9a3338ea62fe87918e6b3a4
Alberto Chang <alberto.changl@pucp.pe>
2023-09-09 22:33:21 -0500

    feat: get by slug

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -6,4 +6,17 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ({strapi}) => ({
+    async findOne(ctx) {
+        const {id} = ctx.params;
+
+        const entity = await strapi.db.query('api::event.event').findOne({
+            where: { slug: id },
+            populate: ['image']
+        })
+
+        const sanitizedEntity = await this.sanitizeOutput(entity);
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
